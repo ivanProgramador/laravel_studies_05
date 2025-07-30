0198a240be64a9bbf203e6775df33b77dc35bbb4
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-07-30 02:27:22 -0300

    organizando o resultado em uma tabela

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,7 +9,39 @@ class MainController extends Controller
 {
     public function index(){
 
-        DB::connection()->getPdo();
-        echo "conetado com sucesso";
+       $clients = DB::table('clients')->get();
+       $this->showTableData($clients);
+    }
+
+    private function showRawData($data)
+    {
+        echo'<pre>';
+        print_r($data);
+        echo'</pre>';
+    }
+
+    private function showTableData($data){
+        echo'<table border="1" >';
+        //cabecalho
+        echo'<tr>';
+            foreach($data[0] as $key=>$value){
+                 echo'<th>'.$key.'</th>';
+            }
+        echo'</tr>';
+
+            foreach($data as $row){
+                echo'<tr>';
+                  foreach($row as $key=>$value){
+                 echo'<td>'.$value.'</td>';
+                }
+                echo'</tr>';
+            }
+
+            echo'</table>';
+
+
+
+
+          
     }
 }
