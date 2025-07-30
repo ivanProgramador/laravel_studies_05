e20e62cc56391126c5fbcb96d4ebbe39990d3543
ivanProgramador <ivan-guita@hotmail.com>
2025-07-30 16:18:58 -0300

    queries com Query builder parte 1

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,9 +8,45 @@ use Illuminate\Support\Facades\DB;
 class MainController extends Controller
 {
     public function index(){
+       
+       //devolvendo todos os dados da tabela  
+       // $clients = DB::table('clients')->get();
 
-       $clients = DB::table('clients')->get();
-       $this->showTableData($clients);
+       //apresentando dados em um array associativo 
+       //$clients = DB::table('clients')->get()->toArray();
+
+       //apresentando dados em um array de arrays associativos 
+       // $clients = DB::table('clients')->get()->map(function($item){
+       //    return (array) $item });
+
+       //apresentando dados apartir dos resulttados
+       //   $products = DB::table('products')->get();
+       //   foreach($products as $product){
+       //      echo $product->product_name.'<br>';
+       //  }
+
+       //obter apenas algumas coluna, no caso abaixo eu so quero nome e preço 
+       // $products = DB::table('products')->get(['product_name','price']);
+       // $this->showTableData($products);
+
+       //obter somente a primeira linha do resultado 
+       //$products = DB::table('products')->get()->first();
+
+       // //obter somente a ultima linha do resultado 
+      // $products = DB::table('products')->get()->last();
+
+       $this->showRawData($products);
+
+       
+
+
+       
+       
+       
+
+
+       
+
     }
 
     private function showRawData($data)
