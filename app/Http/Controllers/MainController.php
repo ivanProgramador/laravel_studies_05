e53d9dafbeac911155cf1813b67a0e98de4144c4
ivanProgramador <ivan-guita@hotmail.com>
2025-07-30 21:59:43 -0300

    criando queries com configuracoes especificas

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Contracts\Database\Query\Builder;
 class MainController extends Controller
 {
     public function index(){
@@ -27,15 +27,71 @@ class MainController extends Controller
      //               ->where('product_name','like','A%')
      //               ->get();
 
-      
-      
+
+     //nesse caso eu posso inserir mais condições dentro do where usando um array de condições  
+     //no caso as condiçoes ficam dentro de um sub array
+
+
+     //   $products = DB::table('products')
+     //                 ->where([
+     //                     ['price','>',50],
+     //                     ['product_name','like','A%']
+     //                 ]
+     //                 )
+     //                 ->get();
+
+
+     //Aqui eu uso a função orWhere para fazer buscas mais complexas
+
+     //   $products = DB::table('products')
+     //                 ->where('price','>',90)
+     //                 ->orWhere(function(Builder $query){
+     //                    $query->where('product_name','Banana')
+     //                          ->orWhere('product_name','cereja');
+     //                 })->get();
+
+
+    //excluindo uma possibilidades da busca, na busca abaixo eu não quero nenhum porduto que o nome comece com a letra M 
+
+    $products = DB::table('products')->where('product','not like','M%')->get();
+ 
+    //aqui eu faço a amesa cois so que usando a função 
+
+    $products = DB::table('products')->whereNot('product','like','M%')->get();
+
+    //selecionando produtos com o preço entre 2 valores
+
+    $products = DB::table('products')->whereBetween('price',[25,50])->get();
+
+    //selecionando produtos que não estão como preço entre 2 valores
+
+    $products = DB::table('products')->whereNotBetween('price',[25,50])->get();
+
+    //selecionando produtos onde os ids estão entre 1,2 e 3
+
+    $products = DB::table('products')->whereIn('id',[1,2,3])->get();
 
     
-      
-      
+
+    
 
 
-      
+
+
+    
+    
+
+
+    
+     
+
+
+
+
+
+
+
+    
        $this->showTableData($products);
 
     }
