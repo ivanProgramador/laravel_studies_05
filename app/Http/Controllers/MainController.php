10325c86834f931c2dae7a49e51f1edf0147f460
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-08-01 07:33:31 -0300

    hard delete e soft delete com query builder

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Contracts\Database\Query\Builder;
@@ -9,14 +10,42 @@ class MainController extends Controller
 {
     public function index(){
 
+       //UPDATE
                   
-        DB::table('clients')
-            ->where('id',1)
-            ->update([
-                'client_name' =>'alterado',
-                'email'=>'alterado@gmal.com'
-            ]);
+       // DB::table('clients')
+       //     ->where('id',1)
+       //     ->update([
+       //         'client_name' =>'alterado',
+       //         'email'=>'alterado@gmal.com'
+       //     ]);
 
+
+       //DELETE 
+       //existem 2 tipos de delete o hard e o soft 
+       //o soft delete é quando o registro é marcado como excluido, mas ainda existe no banco de dados
+       //ele consiste em apenas adicionar uma data ao campo deleted_at  e eu posso fazr uma logica onde
+       //o sistema não mostra o registro ao usuario mas ele permanece lá 
+
+       //hard delete
+       //  é quando o registro é realmente excluido do banco de dados, não existe mais 
+       //   DB::table('users')
+       //   ->where('id',10)
+       //   ->delete();
+
+
+       //soft delete
+       //  é quando o registro é marcado como excluido, mas ainda existe no banco de dados 
+       //abaixo eu usei a função now da lib carbon para adicionar a data atual no campo deleted_at
+       //para que o soft delete funcione, é necessário que a tabela tenha o campo deleted_at
+       //e o campo deleted_at deve ser do tipo timestamp ou datetime 
+
+       
+       DB::table('users')
+          ->where('id',10)
+          ->update([
+                'deleted_at' => Carbon::now()
+          ]);
+       
     }
 
     private function showRawData($data)
