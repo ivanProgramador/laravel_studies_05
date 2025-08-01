0b44c80d768f4ccfc49f77727ece8ee4630946f9
ivanProgramador <ivan-guita@hotmail.com>
2025-07-31 21:56:51 -0300

    formas de fazer update com query builders

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,43 +9,13 @@ class MainController extends Controller
 {
     public function index(){
 
-    
-
-    /* 
-       $count = DB::table('products')->count();
-       $max_price = DB::table('products')->max('price');
-       $min_price = DB::table('products')->min('price');
-       $avg_price = DB::table('products')->avg('price');
-       $sum_prices = DB::table('products')->sum('price');
-    
-       echo'<pre>';
-       print_r([
-         'count' =>$count,
-         'max_price'=> $max_price,
-         'min_price'=> $min_price,
-         'avg_price'=> $avg_price,
-         'sum_prices'=> $avg_price,
-        ]);
-        echo'</pre>';
-      */  
-       
-     
-
-     //usando o order by para controlar a aordem dos resultados 
-
-     $resultados = DB::table('products')
-                   ->orderBy('price','desc')
-                   ->limit(3)
-                   ->get();
-
-    $this->showTableData($resultados);
-
-    
-
-
-
-
-
+                  
+        DB::table('clients')
+            ->where('id',1)
+            ->update([
+                'client_name' =>'alterado',
+                'email'=>'alterado@gmal.com'
+            ]);
 
     }
 
